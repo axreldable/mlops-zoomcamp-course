b0b3d08a6adf17c95d86867330afa1a775a72574
axreldable <aleksei.starikov.ax@gmail.com>
2022-09-17 17:23:35 +0200

    Add github ci demo

--- mini_project/project.py
+++ mini_project/project.py
@@ -1,5 +1,33 @@
+import base64
+import json
+
+
 def get_name(event):
     return event["name"]
+
+
+def base64_decode(encoded_data):
+    decoded_data = base64.b64decode(encoded_data).decode("utf-8")
+    json_event = json.loads(decoded_data)
+    return json_event
+
+
+def lambda_handler(event):
+    print(json.dumps(event))
+
+    result = []
+
+    for record in event["Records"]:
+        encoded_data = record["kinesis"]["data"]
+        json_event = base64_decode(encoded_data)
+
+        print(json_event)
+        name = get_name(json_event)
+        result.append(name)
+
+    return {
+        "result": result
+    }
 
 
 if __name__ == "__main__":
--- mini_project/tests/test_project.py
+++ mini_project/tests/test_project.py
@@ -1,4 +1,4 @@
-from mini_project.project import get_name
+from mini_project.project import get_name, base64_decode, lambda_handler
 
 
 def test_get_name():
@@ -8,5 +8,37 @@ def test_get_name():
     rez = get_name(event)
 
     expected_rez = "name_value"
+
+    assert expected_rez == rez
+
+
+def test_base64_decode():
+    data = "ewogICAgICAgICJuYW1lIjogIm5hbWVfdmFsdWUiCiB9"
+
+    rez = base64_decode(data)
+
+    expected_rez = {
+        "name": "name_value"
+    }
+
+    assert expected_rez == rez
+
+
+def test_lambda_handler():
+    event = {
+        "Records": [
+            {
+                "kinesis": {
+                    "data": "ewogICAgICAgICJuYW1lIjogIm5hbWVfdmFsdWUiCiB9"
+                }
+            }
+        ]
+    }
+
+    rez = lambda_handler(event)
+
+    expected_rez = {
+        "result": ["name_value"]
+    }
 
     assert expected_rez == rez
